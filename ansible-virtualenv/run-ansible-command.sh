22770f9d031b9b6d8bf1d04c5053a393bb6374ec
Guzman <guzman@guzman.fi>
2021-07-25 11:37:33 +0200

    run-ansible-command: Improve handling of not-found commands
    Otherwise we end up in an infinite loop if ansible has not been installed
    in the virtualenv

--- ansible-virtualenv/run-ansible-command.sh
+++ ansible-virtualenv/run-ansible-command.sh
@@ -3,15 +3,16 @@
 set -euo pipefail
 
 DIR=$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" && pwd -P)
+VENV_BIN_DIR=${DIR}/bin  # This script should be placed in the root of a virtualenv
 
-export PATH="${DIR}/bin:${PATH}"
-
-ansible_command=$(basename "$0")
-if ! grep -q '^ansible' <<< "${ansible_command}"; then
-    echo "ERROR: not an ansible command: ${ansible_command}"
+candidate_binary=${VENV_BIN_DIR}/$(basename "$0")
+if ! [ -x "${candidate_binary}" ]; then
+    echo "ERROR: ${candidate_binary} does not exist"
     exit 1
 fi
 
-args=${@:-}
+# Other binaries might be needed by the current one
+export PATH="${VENV_BIN_DIR}:${PATH}"
 
-exec "${ansible_command}" ${args}
+args=${@:-}
+exec "${candidate_binary}" ${args}
